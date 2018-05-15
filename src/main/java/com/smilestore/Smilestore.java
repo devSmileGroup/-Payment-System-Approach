0753f81e7e36b9ba0a56b054f63c8a5df7fdc042
StasOmelchenko <omelchenko.stas.1997@gmail.com>
2018-05-15 03:31:28 +0300

    Initial commit

---
+++ src/main/java/com/smilestore/Smilestore.java
@@ -0,0 +1,13 @@
+package com.smilestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smilestore {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Smilestore.class, args);
+	}
+
+}
